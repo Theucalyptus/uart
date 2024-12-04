33427f2b59bdae2ec659d1b39560c1d027f7a66e
Theucalyptus <28013186+Theucalyptus@users.noreply.github.com>
2024-12-04 17:04:11 +0100

    feat: component clkUnit

--- components/clkUnit/clkUnit.vhd
+++ components/clkUnit/clkUnit.vhd
@@ -11,7 +11,29 @@ entity clkUnit is
 end clkUnit;
 
 architecture behavorial of clkUnit is
-
+  constant facteur : natural := 16;
 begin
+
+  enableRX <= clk and reset;
+
+  process(clk, reset) is
+    variable cpt : integer range 0 to facteur-1 := 0;
+  begin
+    if reset = '0' then
+		enableTX <= '0';
+      cpt := 0;
+    elsif rising_edge(clk) then 
+      if cpt = 15 then
+        enableTX <= '1';
+      else
+        enableTX <= '0';
+      end if;
+
+      cpt:= cpt + 1;
+      if cpt = 16 then
+        cpt := 0;
+      end if;
+    end if;
+  end process;
 
 end behavorial;
