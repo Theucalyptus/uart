1b12146c2a6dd81d43877c683f307e4d58fcf57e
Evann DREUMONT <53308142+LeGmask@users.noreply.github.com>
2024-12-05 09:39:35 +0100

    feat(clkUnit): add comments

--- components/clkUnit/clkUnit.vhd
+++ components/clkUnit/clkUnit.vhd
@@ -2,38 +2,42 @@ LIBRARY IEEE;
 USE IEEE.std_logic_1164.ALL;
 
 ENTITY clkUnit IS
-
   PORT (
     clk, reset : IN STD_LOGIC;
     enableTX   : OUT STD_LOGIC;
-    enableRX   : OUT STD_LOGIC);
-
+    enableRX   : OUT STD_LOGIC
+  );
 END clkUnit;
 
 ARCHITECTURE behavorial OF clkUnit IS
+  -- Facteur de division de la fréquence
   CONSTANT facteur : NATURAL := 16;
 BEGIN
-
   enableRX <= clk AND reset;
 
   PROCESS (clk, reset) IS
+    -- Variable interne pour compter les cycles
     VARIABLE cpt : INTEGER RANGE 0 TO facteur := 0;
   BEGIN
     IF reset = '0' THEN
-      enableTX <= '0';
-      cpt := 0;
+      -- Etat de reset
+      enableTX <= '0'; -- Désactivation de la transmission
+      cpt := 0;        -- Remise à zéro du compteur
     ELSIF rising_edge(clk) THEN
-      IF cpt = 15 THEN
+      enableTX <= '0'; -- Désactivation de la transmission
+
+      IF cpt = facteur - 1 THEN
+        -- Avant dernier cycle
+        -- Activation de la transmission
         enableTX <= '1';
-      ELSE
-        enableTX <= '0';
       END IF;
 
+      -- Incrémentation du compteur
       cpt := cpt + 1;
-      IF cpt = 16 THEN
+      IF cpt = facteur THEN
+        -- Si le compteur atteint la valeur maximale, on le remet à zéro
         cpt := 0;
       END IF;
     END IF;
   END PROCESS;
-
 END behavorial;
